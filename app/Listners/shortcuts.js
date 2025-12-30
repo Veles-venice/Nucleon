2bfe919ac21b05ac0cbee2b38a2942a4c9512998
Roshan Mishra <mishraroshan1101@gmail.com>
2025-12-30 13:14:19 +0530

    Add keyboard shortcut listener for Ctrl+K

---
+++ app/Listners/shortcuts.js
@@ -0,0 +1,14 @@
+window.addEventListener("keydown",(e)=>{
+  try{
+    if(e.ctrlKey && e.key.toLowerCase() === "k"){
+      e.preventDefault();
+      console.log("ctrl+k clicked");
+    }
+  }
+
+  catch(err){
+    console.log("an error encountered!")
+  }
+)};
+
+
