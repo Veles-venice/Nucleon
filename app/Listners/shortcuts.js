fa411b3e9de665bf62c7558176545f5321fc6bf6
Roshan Mishra <mishraroshan1101@gmail.com>
2025-12-30 13:15:28 +0530

    Refactor keydown event listener syntax

--- app/Listners/shortcuts.js
+++ app/Listners/shortcuts.js
@@ -1,14 +1,10 @@
-window.addEventListener("keydown",(e)=>{
-  try{
-    if(e.ctrlKey && e.key.toLowerCase() === "k"){
+window.addEventListener("keydown",(e) => {
+  try {
+    if (e.ctrlKey && e.key.toLowerCase() === "k") {
       e.preventDefault();
       console.log("ctrl+k clicked");
     }
+  } catch (err) {
+    console.log("an error encountered!");
   }
-
-  catch(err){
-    console.log("an error encountered!")
-  }
-)};
-
-
+});
